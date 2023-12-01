d22dec9a14ea2b30adf98613388aad684b14d3f5
esmael almanaseer <esmael almanaseer@DESKTOP-544600A>
2023-12-01 20:43:14 +0300

    Add project files.

---
+++ BookAPI/Models/BookDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BookAPI.Models
+{
+    public partial class BookDBContext : DbContext
+    {
+     
+
+        public BookDBContext(DbContextOptions<BookDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Book { get; set; }
+
+
+    }
+}
